0f12a893d528bcdc1c9424b99780508f145e2eaa
Lucky7Tb <Luckytri101@gmail.com>
2021-09-21 11:04:09 +0700

    add plugin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filters/http-exception.filter';
 import { PrismaExceptionFilter } from './commons/filters/prisma-exception.filter';
 import { ResponseInterceptor } from './commons/interceptors/response.interceptor';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +20,25 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter, new PrismaExceptionFilter);
   
   // Response Inteceptor
-  app.useGlobalInterceptors(new ResponseInterceptor)
+  app.useGlobalInterceptors(new ResponseInterceptor);
+
+  // Helmer
+  app.use(helmet());
+
+  // Comporession
+  app.use(compression());
+
+  // Cors
+  app.enableCors();
+
+  // Swagger
+   const config = new DocumentBuilder()
+    .setTitle('Cheva song')
+    .setDescription('List endpoint of cheva song')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
